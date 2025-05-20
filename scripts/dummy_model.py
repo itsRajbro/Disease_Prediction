121b9d0512740b816f9328611c267499520f4ec0
Ayush <113386990+itsRajbro@users.noreply.github.com>
2025-05-20 14:28:16 +0530

    Delete dummy_model.py

--- scripts/dummy_model.py
+++ scripts/dummy_model.py
@@ -1,19 +1,1 @@
-import tensorflow as tf
-from tensorflow.keras import layers
 
-# Create a simple dummy model
-model = tf.keras.Sequential([
-    layers.Input(shape=(224, 224, 3)),  # Input size same as preprocess
-    layers.Conv2D(16, (3,3), activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(2, activation='softmax')  # 2 classes (just for dummy)
-])
-
-# Compile the model
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-# Save the model
-model.save("model/dummy_model.h5")
-
-print("✅ Dummy model created successfully!")
